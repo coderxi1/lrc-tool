42a45763d0050681abd624c2f45a209b660c4d0b
coderxi1 <coderxi@qq.com>
2025-09-16 01:10:36 +0800

    feat(to): editor page

--- app/components/Editor/LrcTimeMarker.vue
+++ app/components/Editor/LrcTimeMarker.vue
@@ -5,34 +5,30 @@ const props = defineProps<{
 }>()
 
 const emit = defineEmits(['update:modelValue'])
-
-const lines = ref<LrcLine[]>([])
+const lrcObj = reactive(new Lrc())
 const updateModelValue = ()=> {
-  const lrc = new Lrc()
-  lrc.lines = lines.value
-  emit('update:modelValue',lrc.format({meta:true}))
+  emit('update:modelValue',lrcObj.format({meta:true}))
 }
 
-watch(() => props.modelValue, (lrc) => { lines.value = Lrc.from(lrc.trim()).lines.filter(l => l.raw) }, { immediate: true })
+watch(() => props.modelValue, (lrc) => Object.assign(lrcObj,Lrc.from(lrc.trim())), { immediate: true })
 
 const root = ref<HTMLDivElement>()
 const focusLine = (index: number) => root.value?.children[index]?.querySelector('input')?.focus()
 
 const setTime = (line: LrcLine, index: number) => {
   line.time = props.time
-  if (index < lines.value.length - 1) {
+  if (index < lrcObj.lines.length - 1) {
     focusLine(index + 1)
   }
   updateModelValue()
 }
-
 </script>
 
 <template>
   <div ref="root" class="text-lg overflow-y-auto max-h-185">
-    <div v-for="(line, i) in lines" :key="i" class="flex items-center justify-between">
+    <div v-for="(line, i) in lrcObj.lines" :key="i" class="flex items-center justify-between">
       <span class="w-16 text-sm text-maincolor text-right mr-2 font-bold">{{ Lrc.formatTime(line.time) }}</span>
-      <input type="text" class="flex-1 bg-transparent outline-0 focus:b-b" @keydown.enter="setTime(line, i)"
+      <input type="text" class="flex-1 bg-transparent outline-0 focus:b-b" @keydown="e=>e.preventDefault()" @keydown.enter="setTime(line, i)"
         :value="line.text" />
     </div>
   </div>
